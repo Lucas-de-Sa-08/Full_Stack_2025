0c080de631e318c5fb1c21d4972a4bb8b6ff5500
Lucas Lopes de Sá <lucaslopesdesa8@gmail.com>
2025-03-30 20:28:38 -0300

    .

--- Lab_04/guess.js
+++ Lab_04/guess.js
@@ -1,3 +1,37 @@
 //let r = Math.random() * 100;
 //let i = Math.floor(r);
 //window.alert(i)
+
+let numSecreto = Math.floor(Math.random() * 100) + 1;
+let tentativasGrandes = [];
+let tentativasPequenas = [];
+
+function verificarNum(){
+    let numDig = parseInt(document.getElementById('numero').value);
+    let mensagem = document.getElementById('mensagem');
+    let tentativasG = document.getElementById('tentativasGrandes');
+    let tentativasP = document.getElementById('tentativasPequenas');
+
+    if (numDig < 1 || numDig >100){
+        mensagem.textContent = "Por favor, digite um número válido!";
+        mensagem.style.color = "red";
+        return;
+    }
+
+    if (numDig < numSecreto){
+        mensagem.textContent = "Número muito pequeno!";
+        mensagem.style.color = "red"
+        tentativasPequenas.push(numDig);
+    }
+    else if (numDig > numSecreto){
+        mensagem.textContent = "Número muito grande!";
+        mensagem.style.color - "red"
+        tentativasGrandes.push(numDig);
+    }
+    else {
+        mensagem.textContent = "Parabéns, você encontrou o número secreto!";
+        mensagem.style.color = "green"
+    }
+    tentativasP.textContent = tentativasPequenas.join(', ');
+    tentativasG.textContent = tentativasGrandes.join(', ');
+}
